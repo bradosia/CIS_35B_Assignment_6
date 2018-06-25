653a35e70b7194f81c779115796b25d2672deb09
bradosia <bradosia@gmail.com>
2018-06-24 20:48:34 -0700

    save

--- src/driver/Driver.java
+++ src/driver/Driver.java
@@ -2,36 +2,55 @@ package driver;
 
 //import model.Automobile;
 import adapter.*;
-import scale.EditOptions;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		/* This test driver is similar to Hello.java */
+	/**
+	 * The purpose of this driver is to demonstrate: 
+	 * 1. How automobiles are added from text files
+	 * 2. Updating option sets and options
+	 * 3. Serializing automobiles
+	 * 4. Deserializing automobiles
+	 * 5. Get automobile option set and option names and prices
+	 */
+	public static void main() {
 		BuildAuto buildAutoInterface = new BuildAuto();
 		buildAutoInterface.init(); // only call this once
 		// Build Automobile Object from a file.
 		String FordZTWAutomobileKey = buildAutoInterface.buildAuto("FordZTW.txt", "text");
-		// String BMW320iAutomobileKey = buildAutoInterface.buildAuto("BMW320i.txt"); //
-		// unused
+		String BMW320iAutomobileKey = buildAutoInterface.buildAuto("BMW320i.txt", "text");
 		if (FordZTWAutomobileKey != null) {
 			// Print attributes before serialization
 			buildAutoInterface.printAuto(FordZTWAutomobileKey);
-			/* We will start a bunch of operation threads to demonstrate multi-threading
-			 * Some operations depend on the order and silently throw an exception that will
-			 * be logged */
-			String input[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic",
-				"Cool California Blue" };
-			String input2[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic",
-				"Sunshine Hawaiian Gold" };
-			String input3[] = { FordZTWAutomobileKey, "Color", "Colors" };
-			String input4[] = { FordZTWAutomobileKey, "Color", "French Blue Clearcoat Metallic" };
-			buildAutoInterface.operation(0, input); // Updates option name
-			buildAutoInterface.operation(0, input2); // Updates option name
-			buildAutoInterface.operation(1, input3); // Updates option set name
-			buildAutoInterface.operation(3, input4); // set color choice
+			// update an options set's name
+			buildAutoInterface.updateOptionSetName(FordZTWAutomobileKey, "Color", "Colors");
+			// update an options set's option price
+			buildAutoInterface.updateOptionPrice(FordZTWAutomobileKey, "Transmission", "automatic", 50);
+			// choose a transmission option
+			buildAutoInterface.setOptionChoice(FordZTWAutomobileKey, "Transmission", "manual");
+			// Serialize the object
+			buildAutoInterface.serialize(FordZTWAutomobileKey, "FordZTW.data");
 		} else {
 			System.out.println("Could not build automobile");
+		}
+		// Deserialize the object and read it into memory.
+		String FordZTWAutomobileKey2 = buildAutoInterface.deserialize("FordZTW.data");
+		// Print new attributes
+		if (FordZTWAutomobileKey2 != null) {
+			// Print attributes after deserialization
+			buildAutoInterface.printAuto(FordZTWAutomobileKey2);
+			// Print transmission choice
+			String optionName = buildAutoInterface.getOptionChoice(FordZTWAutomobileKey, "Transmission");
+			if (optionName != null) {
+				System.out.println("Transmission choice: " + optionName);
+			}
+			// Print transmission choice price
+			Double optionPrice = buildAutoInterface.getOptionChoicePrice(FordZTWAutomobileKey, "Transmission");
+			if (optionName != null) {
+				System.out.println("Transmission choice price: $" + optionPrice);
+			}
+		} else {
+			System.out.println("could not deserialize automobile");
 		}
 	}
 
